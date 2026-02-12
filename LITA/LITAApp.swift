4412b63fe90122b6517e44ef433be3c7fc03500a
include-steve-kx <kaizexiejobs@gmail.com>
2026-02-11 21:54:00 -0500

    Initial Commit

---
+++ LITA/LITAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LITAApp.swift
+//  LITA
+//
+//  Created by Steve KX on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LITAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LITATests/LITATests.swift
@@ -0,0 +1,17 @@
+//
+//  LITATests.swift
+//  LITATests
+//
+//  Created by Steve KX on 2/11/26.
+//
+
+import Testing
+@testable import LITA
+
+struct LITATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
